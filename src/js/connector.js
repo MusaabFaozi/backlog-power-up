cb064a399fda4b0de3957d75c6b80c9d42f47fba
MusaabFaozi <mosab.albakry@gmail.com>
2024-04-30 15:35:42 -0700

    added a placeholder board button

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,10 +1,10 @@
 console.log("Hello World!");
 
 window.TrelloPowerUp.initialize({
-    "card-buttons": function(t, options) {
+    "board-buttons": function(t, options) {
         return [{
             icon: "https://cdn-icons-png.flaticon.com/512/5360/5360758.png",
-            text: "Hello, World!",
+            text: "Backlog Items",
             callback: function (t) {
                 return t.card().then(card => alert("Hello, there!"))
             }
