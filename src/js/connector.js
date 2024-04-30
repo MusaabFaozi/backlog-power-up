82420655a72e62c8d9666b9511598448a451f724
MusaabFaozi <mosab.albakry@gmail.com>
2024-04-29 17:40:10 -0700

    added button with alert for testing

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,13 @@
 console.log("Hello World!");
 
 window.TrelloPowerUp.initialize({
-    "card-badges": function(t, options) {
-        return [];
+    "card-buttons": function(t, options) {
+        return [{
+            icon: "https://cdn-icons-png.flaticon.com/512/5360/5360758.png",
+            text: "Hello, World!",
+            callback: function (t) {
+                return t.card().then(card => alert("Hello, there!"))
+            }
+        }];
     }
 });
