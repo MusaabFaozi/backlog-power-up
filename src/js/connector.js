c6ab063d3e66844ec3fbde66d6267dc69bc1db53
MusaabFaozi <mosab.albakry@gmail.com>
2024-04-30 15:49:47 -0700

    fixing the board retrieval

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,7 +6,7 @@ window.TrelloPowerUp.initialize({
             icon: "https://cdn-icons-png.flaticon.com/512/5360/5360758.png",
             text: "Backlog Items",
             callback: function (t) {
-                return t.card().then(card => alert("Hello, there!"))
+                return t.board().then(board => alert("Hello, there!"))
             }
         }];
     }
