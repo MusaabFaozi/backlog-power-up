c7ebcc160e48df7bbdb863fb8f36b2f9e393c0e2
MusaabFaozi <mosab.albakry@gmail.com>
2024-10-01 16:48:09 -0700

    added api keys and tokens test

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+const apiKey = process.env.NETLIFY_TRELLO_API_KEY;
+const token = process.env.NETLIFY_TRELLO_BACKLOG_TOKEN;
+
+console.log("apiKey", apiKey);
+console.log("token", token);
 
 var backlog_all = function (t) {
 
