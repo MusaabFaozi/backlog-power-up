6d8adcea13962835fab3b32f6f494aa2a80dec61
MusaabFaozi <mosab.albakry@gmail.com>
2024-10-01 23:05:04 -0700

    adding backlog list retrieval

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,19 +2,33 @@ require('dotenv').config();
 const apiKey = process.env.TRELLO_API_KEY;
 const token = process.env.TRELLO_BACKLOG_TOKEN;
 
+// Settings config
+const BACKLOG_LIST_NAME = "backlog";
+const AUXILIARY_LISTS = ["done today", ]
+
 
 var backlog_all = function(t) {
 
-    console.log(t.lists());
-
-    return t.board("id", "name")
+    return t.board("all")
     .then(function(board) {
 
         const boardid = board.id;
         const boardname = board.name;
+        const backlog_list = board.lists.find(list => list.name.toLowerCase() === 'backlog');
+
         console.log("board id: ", boardid);
         console.log("board name: ", boardname);
+        console.log("backlog list: ", backlog_list);
 
+        if(!backlog_list) {
+            return t.popup({
+                title: 'Error',
+                items: [
+                  { text: 'Backlog list is not found on this board.', callback: function(t) { return t.closePopup(); } }
+                ]
+              });
+        }
+        
     })
 }
 
