6c4241f37926d084f5f0c1969af78b8e9d459f6f
MusaabFaozi <mosab.albakry@gmail.com>
2025-05-11 17:01:20 -0700

    changed checklistId to id

--- src/js/functions/utils.js
+++ src/js/functions/utils.js
@@ -193,7 +193,7 @@ const get_incomplete_checklist_items = async (card) => {
     const checklists = await response.json();
     const incomplete_items = checklists.flatMap(checklist => checklist.checkItems)
         .filter(item => item.state === 'incomplete')
-        .map(async item_1 => { return { cardName: card.name, name: item_1.name, idCard: card.id, checklistId: item_1.id, listId: card.idList}});
+        .map(async item_1 => { return { id: item_1.id, cardName: card.name, name: item_1.name, idCard: card.id, listId: card.idList}});
     return incomplete_items;
 };
 
